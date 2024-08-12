71d157a07e881ae55ba6a24b5d32ff31bff71465
Ranjan <vermaranjan9964@gmail.com>
2024-08-12 12:57:50 +0530

    ChatBox leftSideBar UI completed

---
+++ chat-app/src/Components/RightSideBar/RightSideBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import "./RightSideBar";
+const RightSideBar = () => {
+  return <div></div>;
+};
+
+export default RightSideBar;
--- chat-app/src/Pages/Chat/Chat.jsx
+++ chat-app/src/Pages/Chat/Chat.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Chat.css";
-
+import ChatBox from "../../Components/ChatBox/ChatBox";
+import RightSideBar from "../../Components/RightSideBar/RightSideBar";
+import LeftSideBar from "../../Components/LeftSideBAr/LeftSideBar";
 const Chat = () => {
-  return <div>thisnis chat page</div>;
+  return (
+    <div className="chat">
+      <div className="chat-container">
+        <LeftSideBar></LeftSideBar>
+        <ChatBox></ChatBox>
+        <RightSideBar></RightSideBar>
+      </div>
+      ;
+    </div>
+  );
 };
 
 export default Chat;
