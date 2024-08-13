0090f13f955ddc4bf36e0f8f4f6b79c4e426cdad
Ranjan <vermaranjan9964@gmail.com>
2024-08-14 00:49:23 +0530

    UI Design Completed

--- chat-app/src/Pages/ProfileUpdate/ProfileUpdate.jsx
+++ chat-app/src/Pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./ProfileUpdate.css";
+import assets from "../../assets/assets";
 
 const ProfileUpdate = () => {
-  return <div>we r on th profile page</div>;
+  return (
+    <div className="profile">
+      <div className="profile-container">
+        <form>
+          <h3>Profile Detals</h3>
+          <label htmlFor="avatar">
+            <input type="file" id="avatar" accept=".png, .jpg, .jpeg " hidden />
+            <img src={assets.avatar_icon} alt="" />
+            upload profile picture
+          </label>
+          <input type="text" placeholder="Your name " required />
+          <textarea placeholder="Write Profile Bio " required></textarea>
+          <button type="submit">Save</button>
+        </form>
+        <img className="profile-pic" src={assets.logo_icon} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default ProfileUpdate;
